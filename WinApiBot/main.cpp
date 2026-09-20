6da32f3a4c7c7c788852bfa75980a3a75792b29b
sedrias <syxolorid@gmail.com>
2026-09-21 01:43:30 +0300

    Open token file next to the executable

--- WinApiBot/main.cpp
+++ WinApiBot/main.cpp
@@ -11,6 +11,19 @@ bool PrintLine(const wchar_t* text) {
 }
 
 int wmain() {
-	if (!PrintLine(L"Привет, Мир!")) return 1;
+	wchar_t exePath[MAX_PATH];
+	if (!GetModuleFileNameW(NULL, exePath, MAX_PATH)) return 1;
+	wchar_t* lastSlash = wcsrchr(exePath, L'\\');
+	if (!lastSlash) return 1;
+	lastSlash[1] = L'\0';
+	wcscat_s(exePath, MAX_PATH, L"token.txt");
+	PrintLine(exePath);
+	HANDLE hTokenFile = CreateFileW(exePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hTokenFile == INVALID_HANDLE_VALUE) {
+		PrintLine(L"Не удалось открыть token.txt");
+		return 1;
+	}
+	PrintLine(L"Файл token.txt открыт");
+	CloseHandle(hTokenFile);
 	return 0;
 }
